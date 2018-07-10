105a6e973c4e9cc6cf9a71b0a35d68399d899d9d
shofi384@gmail.com Rahman <shofi384@gmail.com>
2018-07-10 20:22:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instructions) {
   var empty = new Array();
   for(var i=0; i<musicians.length; i++) {
-    empty.push(musicians[i] + instructions[i]);
+    empty.push(musicians[i] + ' plays ' + instructions[i]);
   }
   return empty;
 }
