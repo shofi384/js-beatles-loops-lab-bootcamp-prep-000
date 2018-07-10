f3990a94ffe2d929596a31b287cb47b40babf08d
shofi384@gmail.com Rahman <shofi384@gmail.com>
2018-07-10 20:19:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// add solution here
+function theBeatlesPlay(musicians, instructions) {
+  var empty = new Array();
+  for(var i=0; i<musicians.length; i++) {
+    empty.push(musicians[i] + instructions[i]);
+  }
+  return empty;
+}
